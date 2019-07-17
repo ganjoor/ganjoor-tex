aa1ea0721d087d75607598e3c394b5d7d9d57a02
rshfsr <rshfsr@gmx.com>
2019-07-17 18:09:32 +0200

    use html.parser

--- txt/html2txt.py
+++ txt/html2txt.py
@@ -1,48 +1,82 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
-import urllib2
+import urllib.request
 
-def writefile(outname, title, outurl, outpath):
-    outf = open(outpath+"/"+outname+".txt", "w")
-    outf.write(title+'\n\n')
-    print(title)
-    response = urllib2.urlopen(outurl)
-    content = response.read().split("\n")
-    for line in content:
-        if line[0:15] == "<div class=\"b\">": 
-            m1 = line.split('>')[3].split('<')[0]
-            m2 = line.split('>')[7].split('<')[0]
-            outf.write(m1+'\n')
-            outf.write(m2+'\n\n')
-    outf.close()
+from html.parser import HTMLParser
+from html.entities import name2codepoint
+
+class PoemParser(HTMLParser):
+    def __init__(self, outf):
+        super(PoemParser, self).__init__()
+        self.tags = []
+        self.out = outf
+        self.divclass = ""
+    def handle_starttag(self, tag, attrs):
+        self.tags.append(tag)
+        for attr in attrs:
+            if attr[0] == "class":
+                self.divclass = attr[1]
+
+    def handle_endtag(self, tag):
+        self.tags.pop()
+
+    def handle_data(self, data):
+        if len(self.tags)>=2 and self.tags[-1] == "p" and self.tags[-2] == "div":
+            if self.divclass == "m1":
+                self.out.write(data+'\n')
+            if self.divclass == "m2":
+                self.out.write(data+'\n\n')
+
+class ContentTableParser(HTMLParser):
+    def __init__(self, outpath):
+        super(ContentTableParser, self).__init__()
+        self.tags = []
+        self.url = ""
+        self.path = outpath
+    def handle_starttag(self, tag, attrs):
+        self.tags.append(tag)
+        for attr in attrs:
+            if attr[0] == "href":
+                self.url = attr[1]
+
+    def handle_endtag(self, tag):
+        self.tags.pop()
+
+    def handle_data(self, data):
+        if len(self.tags)>=2 and self.tags[-1] == "a" and self.tags[-2] == "p" and self.url[0:4]=="http":
+            
+            urllist = self.url.split("/")
+            urllist = list(filter(None, urllist))
+            outname = urllist[len(urllist)-1]
+            if (len(outname)==3):
+                outname = outname[0:2] + "00" + outname[2:3]
+            if (len(outname)==4):
+                outname = outname[0:2] + "0" + outname[2:4]
+
+            self.writefile(data, outname)
+
+    def writefile(self, title, outname):
+        outf = open(self.path+"/"+outname+".txt", "w")
+        outf.write(title+'\n\n')
+        print(title)
+        outcontent = urllib.request.urlopen(self.url).read().decode("utf8")
+
+        parser = PoemParser(outf)
+        parser.feed(outcontent)
+
+        outf.close()
 
 if len(sys.argv) < 3 :
     print("usage: "+sys.argv[0]+" <ganjoor link> <text directory>")
     sys.exit(0)
 
-url = sys.argv[1].split('/')
-name = url[len(url)-1]
-
 outpath = sys.argv[2]
 
-inf = urllib2.urlopen(sys.argv[1])
-content = inf.read().split("\n")
+inf = urllib.request.urlopen(sys.argv[1])
+content = inf.read().decode("utf8")
 
-for line in content:
-    if line[0:3] == "<p>": 
-        line1 = line.split('>')[1]
-        if (not "\"" in line1) or (not "/" in line1):
-            continue
-        outurl = line1.split("\"")[1]
-        outurllist = outurl.split("/")
-        outurllist = filter(None, outurllist)
-        outname = outurllist[len(outurllist)-1]
-        if (len(outname)==3):
-            outname = "sh00" + outname[2:3]
-        if (len(outname)==4):
-            outname = "sh0" + outname[2:4]
-        title = line.split('>')[2].split('<')[0]
-        writefile(outname, title, outurl, outpath) 
+parser = ContentTableParser(outpath)
+parser.feed(content)
 
 inf.close()
